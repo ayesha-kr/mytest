60730bdaf2707f9ccd42849c6f3a4fab930adcde
Ayesha Khaliq <ayeshakhaliqrana@gmail.com>
2017-02-21 12:52:02 +0500

    commit all files.

---
+++ app.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+
+app.get('/books/122', function(req, res) {
+	var reply =
+           {
+              "book_pages" : 123,
+              "book_name" : "worldwar"
+           };
+   res.setHeader('content-Type', 'application/json');
+   console.log(reply, '@@@');
+   res.status(200).send(JSON.stringify(reply));
+
+})
+
+var server = app.listen(3000, function() {
+    console.log(new Date().toISOString() + ": server started on port 3000");
+});
+
+// Extra agrs in actual api are not caught.
